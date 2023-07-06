b0eef7fb355636553b2fd92ed74962001ce467e5
panyijun <panyijun@qiyi.com>
2023-07-06 17:23:33 +0800

    Edit

--- app/src/main/java/com/example/learn/view/EditView.kt
+++ app/src/main/java/com/example/learn/view/EditView.kt
@@ -3,6 +3,7 @@ package com.example.learn.view
 import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -10,6 +11,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
@@ -210,6 +212,7 @@ fun TextEdit() {
         },
         modifier = Modifier
             .fillMaxWidth()
+            .border(1.5.dp, MaterialTheme.colorScheme.primary,RoundedCornerShape(10.dp))
             .onFocusChanged { focusState ->
 
             },
