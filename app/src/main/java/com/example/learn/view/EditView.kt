6c12900a43ec19bc00c5dd9b4a58c9af9c69061d
panyijun <panyijun@qiyi.com>
2023-07-17 17:19:41 +0800

    CustomEdit

--- app/src/main/java/com/example/learn/view/EditView.kt
+++ app/src/main/java/com/example/learn/view/EditView.kt
@@ -1,17 +1,18 @@
 package com.example.learn.view
 
-import android.widget.Toast
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
@@ -21,11 +22,13 @@ import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
@@ -35,6 +38,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
@@ -42,9 +46,11 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import com.example.learn.R
+import com.example.learn.detail.mirroringBackIcon
 
 // https://blog.csdn.net/c10WTiybQ1Ye3/article/details/118771931
 @Composable
@@ -67,6 +73,10 @@ fun EditView() {
         Spacer(modifier = Modifier.height(20.dp))
 
         BasicEdit()
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        CustomEdit()
     }
 }
 
@@ -212,7 +222,7 @@ fun TextEdit() {
         },
         modifier = Modifier
             .fillMaxWidth()
-            .border(1.5.dp, MaterialTheme.colorScheme.primary,RoundedCornerShape(10.dp))
+            .border(1.5.dp, MaterialTheme.colorScheme.primary, RoundedCornerShape(10.dp))
             .onFocusChanged { focusState ->
 
             },
@@ -261,5 +271,91 @@ fun BasicEdit() {
             .padding(horizontal = 10.dp, vertical = 15.dp)
             .fillMaxWidth()
     )
+}
+
+@Composable
+fun CustomEdit() {
+    var edit by remember { mutableStateOf(TextFieldValue("")) }
+    var focused by remember { mutableStateOf(false) }
+
+    SearchBar(
+        query = edit,
+        onQueryChange = { edit = it },
+        searchFocused = focused,
+        onSearchFocusChange = { focused = it },
+        onClearQuery = { edit = TextFieldValue("") },
+    )
+}
+
+@Composable
+private fun SearchBar(
+    query: TextFieldValue,
+    onQueryChange: (TextFieldValue) -> Unit,
+    searchFocused: Boolean,
+    onSearchFocusChange: (Boolean) -> Unit,
+    onClearQuery: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Surface(
+        color = Color(0xFFF0F0F0),
+        contentColor = MaterialTheme.colorScheme.secondary,
+        shape = RoundedCornerShape(10.dp),
+        modifier = modifier
+            .fillMaxWidth()
+            .height(56.dp)
+            .padding(horizontal = 24.dp, vertical = 8.dp)
+    ) {
+        Box(Modifier.fillMaxSize()) {
+            if (query.text.isEmpty() && !searchFocused) {
+                SearchHint()
+            }
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .wrapContentHeight()
+            ) {
+                if (searchFocused) {
+                    androidx.compose.material.IconButton(onClick = onClearQuery) {
+                        androidx.compose.material.Icon(
+                            imageVector = mirroringBackIcon(),
+                            tint = MaterialTheme.colorScheme.primary,
+                            contentDescription = null
+                        )
+                    }
+                }
+                BasicTextField(
+                    value = query,
+                    onValueChange = onQueryChange,
+                    modifier = Modifier
+                        .weight(1f)
+                        .onFocusChanged {
+                            onSearchFocusChange(it.isFocused)
+                        }
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun SearchHint() {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxSize()
+            .wrapContentSize()
+    ) {
+        androidx.compose.material.Icon(
+            imageVector = Icons.Outlined.Search,
+            tint = Color(0xFF000000),
+            contentDescription = null
+        )
+        Spacer(Modifier.width(8.dp))
+        androidx.compose.material.Text(
+            text = "Search",
+            color = MaterialTheme.colorScheme.primary
+        )
+    }
 }
 
